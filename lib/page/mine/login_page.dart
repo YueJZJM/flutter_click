18f300bad6ed20d036e695027cdf80b03b953920
岳剑钟 <yuejzhfut@163.com>
2020-09-16 00:02:41 +0800

    [feature] 实现登录页面 UI

--- lib/page/mine/login_page.dart
+++ lib/page/mine/login_page.dart
@@ -6,6 +6,8 @@ class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
+String _inputAccount = "";
+String _inputPwd = "";
 
 class _LoginPageState extends State<LoginPage> {
   @override
@@ -14,11 +16,13 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         actions: <Widget>[
-          Padding(padding: const EdgeInsets.all(12.0),
-            child: Text("帮助",
-              style: TextStyle(fontSize: 16.0, color: Color(0xff6B91BB)),),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              "帮助",
+              style: TextStyle(fontSize: 16.0, color: Color(0xff6B91BB)),
+            ),
           )
-
         ],
         leading: Padding(
             padding: const EdgeInsets.all(12.0),
@@ -27,37 +31,172 @@ class _LoginPageState extends State<LoginPage> {
               width: 20.0,
               height: 20.0,
             )),
+        elevation: 0,
       ),
-      body: Padding(padding: const EdgeInsets.symmetric(horizontal: 12),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Padding(padding: const EdgeInsets.only(top: 40),),
-            Text("请输入账号密码",style: TextStyle(fontSize: 20),),
-            buildEditText("手机号或邮箱"),
-            buildEditText("密码"),
+            Padding(
+              padding: const EdgeInsets.only(top: 40),
+            ),
+            Text(
+              "请输入账号密码",
+              style: new TextStyle(fontSize: 24.0, color: Colors.black),
+            ),
+            //TODO AccountEditText
+            Container(margin:  const EdgeInsets.only( top: 30.0),
+              //TODO AccountEditText
+              child: buildUserNameEditText("手机号或邮箱"),),
+//            buildEditText("手机号或邮箱"),
+            //TODO PwdEditText
+            buildPasswordEditText("密码"),
+            Padding(padding:  const EdgeInsets.only(top: 60),),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
               children: <Widget>[
-                Expanded(child:
-                  RaisedButton(child: Text("登录"),
+                Expanded(
+                  child: RaisedButton(
+                    child: Text("登 录"),
                     color: Color(0xffFF8200),
                     textColor: Colors.white,
                     disabledTextColor: Colors.white,
                     disabledColor: Color(0xffFFD8AF),
-                  )
+                    onPressed: (_inputAccount.isEmpty || _inputPwd.isEmpty)
+                        ? null
+                        :(){
+                        print(_inputPwd );
+                        print( _inputAccount);
+                    }
+                  ),
                 ),
               ],
             ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text("注册"),
+                ),
+                Expanded(
+                    child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text("忘记密码"),
+                )),
+              ],
+            ),
+            Padding(padding: const EdgeInsets.only(top: 40),),
+            buildOtherLoginWayText(),
+            Padding(padding:  const EdgeInsets.only(top: 150),),
+            buildOtherLoginWay(),
           ],
         ),
       ),
     );
   }
 
-  Widget buildEditText(String text) {
-    return TextField(decoration: InputDecoration(
-        labelText: text
-    ),);
+  Widget buildUserNameEditText(String text) {
+    return TextField(
+      decoration: InputDecoration(labelText: text),
+      onChanged: (v){
+        _inputAccount = v;
+        setState(() {
+
+        });
+      },
+    );
   }
+
+  buildPasswordEditText(String text) {
+    return TextField(
+      decoration: InputDecoration(labelText: text),
+      obscureText:true,
+      onChanged: (v){
+        _inputPwd = v;
+        print(_inputPwd);
+        setState(() {
+
+        });
+      },
+    );
+  }
+
+
+  Widget buildOtherLoginWayText() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: Container(
+            height: 1,
+            color: Color(0xffEAEAEA),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Center(
+            child: Text("其他登录方式"),
+          )
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            height: 1,
+            color: Color(0xffEAEAEA),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget buildOtherLoginWay() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child:Container(),
+          flex: 1,
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Image.asset(
+                    Constant.ASSETS_IMG + 'login_weixin.png',
+                    width: 40.0,
+                    height: 40.0,
+                  ),
+                  Text("微信"),
+                ],
+              ),
+              Container(
+                width: 40,
+              ),
+              Column(
+                children: <Widget>[
+                  Image.asset(
+                  Constant.ASSETS_IMG + 'login_qq.png',
+                  width: 40.0,
+                  height: 40.0,
+                  ),
+                  Text("QQ"),
+                ]
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child:Container(),
+
+          flex: 1,
+        )
+      ],
+    );
+  }
+
 }
+
